da26b16a91f001cc9f75b965ca243545d017ce96
Shigeya Suzuki <shigeya@wide.ad.jp>
2021-05-08 08:37:29 +0900

    Compacting test

--- packages/dns-data/tests/lib/dns_wire.spec.ts
+++ packages/dns-data/tests/lib/dns_wire.spec.ts
@@ -4,22 +4,25 @@ import each from 'jest-each';
 import { domain_name2wire, wire2domain_name } from "../../src/lib/dns_wire";
 
 describe("Domain name wire format conversion library", () => {
-
-    describe.each([
+    const test_vector = [
         ["xp.net.",        "\x02xp\x03net\x00" ],
         ["Z.ISI.ARPA.",    "\x01z\x03isi\x04arpa\x00" ],
         ["FOO.ISI.ARPA.",  "\x03foo\x03isi\x04arpa\x00" ],
         ["ARPA.",          "\x04arpa\x00" ],
         ["ARPA",           "\x04arpa" ],
         ["sh.wide.xx.jp.", "\x02sh\x04wide\x02xx\x02jp\x00" ],
-        ["ns.wide.xx.jp", "\x02ns\x04wide\x02xx\x02jp" ],
-    ])("domain name <%s>", ( domain_name, wire ) =>{
-        it("can translate to wire format", async () => {
-            expect(domain_name2wire(domain_name)).toBe(wire);
-        });
-        it("can translate from wire format", async () => {
-            expect(wire2domain_name(wire)).toBe(domain_name.toLowerCase());
-        });
+        ["ns.wide.xx.jp", "\x02ns\x04wide\x02xx\x02jp" ]
+    ];
+
+    it("can translate to wire format", () => {
+        test_vector.forEach(async (value) => {
+            expect(domain_name2wire(value[0])).toBe(value[1]);
+        })
     });
 
+    it("can translate from wire format", () => {
+        test_vector.forEach(async (value) => {
+            expect(wire2domain_name(value[1])).toBe(value[0].toLowerCase());
+        });
+    });
 });
