a941f1af63c09bfb8077a3d53ff6d3fc49c17e55
Shigeya Suzuki <shigeya@wide.ad.jp>
2021-05-07 20:08:33 +0900

    worksnap

--- packages/dns-data/src/lib/dns_wire.ts
+++ packages/dns-data/src/lib/dns_wire.ts
@@ -1,7 +1,34 @@
 // Converting between DNS wire format and string(utf)
 
-export function domain_name2wire(domainname: string): string {
+export function domain_name2wire(domain_name: string): string {
+    console.log(">>"+domain_name);
     return "X";
+    var x = "";
+    let d = domain_name;
+    let l = d.length;
+    console.log(d);
+    for (var i = 0, j = 0; i < l;) {
+        console.log(i);
+        for (; j< l && d[j] != '.'; ++j) {
+        }
+
+        if (j - i != 0) { // if there is text to copy
+            x += String.fromCharCode(j - i); // length
+            x += d.substring(i, j-1).toLowerCase();
+        }
+
+        if (j < l) {
+            i = j + 1;
+            if (i == l) {
+                x += String.fromCharCode(0);
+            }
+        }
+        else {
+            i = j;
+        }
+    }
+    
+    return x;
 };
 
 export function wire2domain_name(wire: string): string {
--- packages/dns-data/tests/lib/dns_wire.spec.ts
+++ packages/dns-data/tests/lib/dns_wire.spec.ts
@@ -7,17 +7,17 @@ describe("Domain name wire format conversion library", () => {
 
     describe.each([
         ["xp.net.",        "\x02xp\x03net!" ],
-        ["Z.ISI.ARPA.",    "\x01z\x03isi\x04arpa!" ],
-        ["FOO.ISI.ARPA.",  "\x03foo\x03isi\x04arpa!" ],
-        ["ARPA.",          "\x04arpa!" ],
-        ["ARPA",           "\x04arpa" ],
-        ["sh.wide.xx.jp.", "\x02sh\x04wide\x02xx\x02jp!" ],
-        ["ns.wide.xx.jp ", "\x02ns\x04wide\x02xx\x02jp" ],
+        // ["Z.ISI.ARPA.",    "\x01z\x03isi\x04arpa!" ],
+        // ["FOO.ISI.ARPA.",  "\x03foo\x03isi\x04arpa!" ],
+        // ["ARPA.",          "\x04arpa!" ],
+        // ["ARPA",           "\x04arpa" ],
+        // ["sh.wide.xx.jp.", "\x02sh\x04wide\x02xx\x02jp!" ],
+        // ["ns.wide.xx.jp ", "\x02ns\x04wide\x02xx\x02jp" ],
     ])("domain name <%s>", ( domain_name, wire ) =>{
-        it("can translate to wire format", () => {
+        it("can translate to wire format", async () => {
             expect(domain_name2wire(domain_name)).toBe(wire);
         });
-        it("can translate from wire format", () => {
+        it("can translate from wire format", async () => {
             expect(wire2domain_name(wire)).toBe(domain_name);
 
         });
